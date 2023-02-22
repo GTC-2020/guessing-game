f850f693f6e9ea7542c0b41aaa10eb506f48a2d3
George <tsurkan.g@gmail.com>
2023-02-22 16:53:24 +0200

    feat: my solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
-
+    left;
+    rigth;
+    mid;
+    constructor() {
+    }
+    
     setRange(min, max) {
-
+        this.left = min;
+        this.rigth = max;
     }
 
     guess() {
-
+            this.mid = Math.round((this.rigth + this.left) / 2);
+            return this.mid;
     }
 
     lower() {
-
+        this.rigth = this.mid;
     }
 
     greater() {
-
+        this.left = this.mid;
     }
 }
 
